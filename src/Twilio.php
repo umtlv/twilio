2e1a64943b1be272829800f4cf131d05770ad486
Nurislan Umetaliev <nurislan.umetaliev@axelkg.com>
2022-10-10 16:22:07 +0600

    is valid number

--- src/Twilio.php
+++ src/Twilio.php
@@ -11,6 +11,7 @@ class Twilio
 {
     private $twilio;
     private $from;
+    private $US = true;
 
     /**
      * @throws ConfigurationException
@@ -29,9 +30,37 @@ class Twilio
      */
     public function sendSms($to, $message)
     {
+        if (!$this->isValidNumber($to)) {
+            throw new ConfigurationException("Incorrect number");
+        }
+
         return $this->twilio->messages->create($to, [
             'from' => $this->from,
             'body' => $message
         ]);
+    }
+
+    /**
+     * @throws TwilioException
+     */
+    private function isValidNumber($to)
+    {
+        if (empty($to)) return false;
+
+        if ($this->US && !preg_match('/^\+[1-9]\d{1,14}$/', $to))
+            return false;
+
+        $lookup = $this->twilio->lookups->v1->phoneNumbers($to);
+        $number = $lookup->fetch(["type" => ["carrier"]]);
+
+        if ($number['carrier']['type'] === 'mobile')
+            return true;
+
+        return false;
+    }
+
+    public function notOnlyUS()
+    {
+        $this->US = false;
     }
 }
